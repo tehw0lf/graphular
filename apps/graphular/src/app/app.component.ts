4a1f4a35476d57b34a26d3008c2aec3509ad3818
tehw0lf <tehwolf@protonmail.com>
2023-06-04 15:15:24 +0200

    initial commit

---
+++ apps/graphular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graphular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'graphular';
+}
