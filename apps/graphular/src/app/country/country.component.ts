4f9ff993693a590ade5b88cd5978ece658d94569
tehw0lf <tehwolf@protonmail.com>
2025-02-16 20:50:51 +0100

    Update to latest packages

--- apps/graphular/src/app/country/country.component.ts
+++ apps/graphular/src/app/country/country.component.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 import { Country } from './country.type';
 
 @Component({
-  selector: 'graphular-country',
-  templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss'],
+    selector: 'graphular-country',
+    templateUrl: './country.component.html',
+    styleUrls: ['./country.component.scss'],
+    standalone: false
 })
 export class CountryComponent {
   @Input() country: Country | null = null;
