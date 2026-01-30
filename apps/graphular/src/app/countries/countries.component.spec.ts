5ba56a8d121d762e9afe100b2ab71a966c5007b2
tehw0lf <tehwolf@protonmail.com>
2026-01-30 21:47:59 +0100

    chore(dependencies): update Angular and related packages to version 21.1.2
    * Upgraded Angular packages from version 20.0.3 to 21.1.2 for improved performance and features.
    * Updated Apollo Client to version 4.1.3 and apollo-angular to version 13.0.0.
    * Bumped @nx packages to version 22.4.3 for better compatibility and new features.
    * Updated TypeScript and ESLint related packages to their latest versions for enhanced development experience.
    * Ensured compatibility with the latest versions of dependencies like Cypress and Jest.

--- apps/graphular/src/app/countries/countries.component.spec.ts
+++ apps/graphular/src/app/countries/countries.component.spec.ts
@@ -3,9 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-
 import { CountriesComponent } from './countries.component';
-
 const uri = 'https://countries.trevorblades.com/graphql';
 function createApollo(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
@@ -14,7 +12,6 @@ function createApollo(): ApolloClientOptions<any> {
     cache: new InMemoryCache(),
   };
 }
-
 describe('CountriesComponent', () => {
   let fixture: ComponentFixture<CountriesComponent>;
   beforeEach(async () => {
@@ -22,10 +19,8 @@ describe('CountriesComponent', () => {
       declarations: [CountriesComponent],
       providers: [provideApollo(createApollo)],
     }).compileComponents();
-
     fixture = TestBed.createComponent(CountriesComponent);
   });
-
   it('should create the app', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
