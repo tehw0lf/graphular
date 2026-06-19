e62650d5e9b81d3809dd53103d2395fd63da73ad
tehw0lf <tehwolf@protonmail.com>
2026-06-20 00:14:49 +0200

    chore: [nx migration] update-23-1-0-convert-to-flat-config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import nx from '@nx/eslint-plugin';
+
+export default [
+  ...nx.configs['flat/base'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
